db64cfb1d934219c2c8c02d53b2f09898e7541a8
nowshedahmed <[nowshedahmed59@gmail.com]>
2024-09-09 13:22:27 +0600

    todo app compeleted

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,13 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.home,name="home"),
+    path('', views.task_list,name="home"),
+    path('task_update/<int:pe>/', views.task_update,name="task_update"),
+    path('task_delete/<int:pd>/', views.task_delete,name="task_delete"),
 
 
 
 ]
+
+
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,63 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
 # Create your views here.
+from .models import Task
+from .forms import TaskForm
+from django.contrib import messages
 
-def home(request):
-    return render(request,'index.html')
+
+
+
+
+
+
+
+
+
+def task_list(request):
+    lists = Task.objects.all()
+
+    #add task in list
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Task added successfully!")
+            return redirect('home')
+    else:
+        form = TaskForm()
+
+
+    context = {
+        "lists":lists,
+        "form":form,
+    }
+    return render(request,'index.html',context)
+
+
+def task_update(request, pe):
+    task = get_object_or_404(Task, id=pe)
+    
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)  # Pass the task instance here
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Task updated successfully!")
+            return redirect('home')
+    else:
+        form = TaskForm(instance=task)
+
+    context = {
+        "form": form,
+        "task": task,
+    }
+    return render(request, 'update.html', context)
+
+
+def task_delete(request,pd):
+    task = Task.objects.get(id=pd)
+    if request.method == "POST":
+        task.delete()
+        messages.success(request, "Task deleted successfully!")
+        return redirect('home')
+    context = {}
+    return render(request,'delete.html',context)
